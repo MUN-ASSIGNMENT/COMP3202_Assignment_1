dcfc8408f7616d19f97d352bf3d5c275914765b8
yeeteing <ytlo@mun.ca>
2021-01-30 12:38:46 -0330

    update chagnes

--- A1_KNN.py
+++ A1_KNN.py
@@ -5,19 +5,14 @@ import sys
 import random
 import pandas as pd
 
-no_of_observation = 400
 # Should pass in the data as this:: python A1_KNN.py Data4A1_NoDup.tsv 5 10
 
-# tab-delimited plain-text file name.
-file_name = sys.argv[1]
-# the number of closest neighbours (k) in integer.
-k = int(sys.argv[2])
-# the number of unknown instances in integer
-unknown_instance_number = int(sys.argv[3])
-print("file name: ", file_name, "\nnumber of closest neighbours: ",
-      k, "\nnumber of unknown instances", unknown_instance_number)
-df = pd.read_csv(file_name, sep='\t', header=0)
+no_of_observation = 400
+file_name = sys.argv[1] # tab-delimited plain-text file name.
+k = int(sys.argv[2]) # the number of closest neighbours (k) in integer.
+unknown_instance_number = int(sys.argv[3]) # the number of unknown instances in integer
 
+df = pd.read_csv(file_name, sep='\t', header=0) # read the text file
 ## check validity of k and U
 # check if k is in range, if not, print error message
 if k <= 1 and k >= (no_of_observation - unknown_instance_number):
@@ -28,15 +23,9 @@ elif unknown_instance_number > no_of_observation:
 
 # Randomly select U instances to be the unknown instances in UnInstance
 unknownInstance = df.sample(unknown_instance_number)
-print(unknownInstance)
-column = unknownInstance.iloc[:, 0]
-print(unknownInstance.index)
-print(df)
-# Remove the unknown instances in UnInstance from the input data. The remaining
-# instances are the training data T.
-x = df.drop(index=unknownInstance.index,
-            axis=0)
-print(x)
+
+# Remove the unknown instances in UnInstance from the input data. The remaining instances are the training data T.
+T = df.drop(index=unknownInstance.index, axis=0)
 
 # calculate the distance with the chi squared distanca Formula
 
