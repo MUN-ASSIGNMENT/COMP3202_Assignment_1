11a81510028bfb02adab7560c9078e28c8de7f93
yeeteing <ytlo@mun.ca>
2021-02-06 21:13:51 -0330

    clean up

--- A1_KNN.py
+++ A1_KNN.py
@@ -1,36 +1,81 @@
-import csv
-import numpy as np
-import matplotlib.pyplot as plt
+# Andrew Nash - 201609492
+# Yee Teing Lo - 201805462
 import sys
-import random
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from statistics import mode
 
-# Should pass in the data as this:: python A1_KNN.py Data4A1_NoDup.tsv 5 10
-
-no_of_observation = 400
-file_name = sys.argv[1] # tab-delimited plain-text file name.
-k = int(sys.argv[2]) # the number of closest neighbours (k) in integer.
-unknown_instance_number = int(sys.argv[3]) # the number of unknown instances in integer
-
-df = pd.read_csv(file_name, sep='\t', header=0) # read the text file
-## check validity of k and U
-# check if k is in range, if not, print error message
-if k <= 1 and k >= (no_of_observation - unknown_instance_number):
-    print('k should be greater or equal to 1, and less than or equal to # of Observation minus U. \n please re-enter your 2nd argument')
-## check if U is in range, if not, print error message
-elif unknown_instance_number > no_of_observation:
-    print('U should be less than the observation number. \n please re-enter your 3rd argument')
-
-# Randomly select U instances to be the unknown instances in UnInstance
-unknownInstance = df.sample(unknown_instance_number)
-
-# Remove the unknown instances in UnInstance from the input data. The remaining instances are the training data T.
-T = df.drop(index=unknownInstance.index, axis=0)
-
-# calculate the distance with the chi squared distanca Formula
+BONUS = False
 
 
-def chiDistance(x, y):
-    distance = 0.5 * np.sum([((x - y) ** 2) / (x + y)
-                             for (x, y) in zip(x, y)])
-    return distance
+def chi2_dist(X, Y):
+    dist = 0
+    
+    for x, y in zip(X, Y):
+        dist += ((x - y) ** 2) / (x + y + 0.00001) # .01 prevents divide by 0
+
+    return dist * 0.5
+
+
+def knn(train, query, k):
+    neighbors = []
+    
+    for i, row in train.iterrows():
+        dist = chi2_dist(query.drop('Class').tolist(),
+                         row.drop('Class').tolist())
+        neighbors.append((dist, row['Class']))
+    
+    neighbors = sorted(neighbors, key=lambda tup: tup[0])
+
+    neighbors = neighbors[:k]
+    predict = mode([row[-1] for row in neighbors]) # need py >= 3.8 for this
+
+    return predict
+
+def train(x_train, x_test, k):
+    correct = 0
+    for i, row in x_test.iterrows():
+        predict = knn(x_train, row, k)
+        
+        if predict == row['Class']:
+            correct += 1
+            
+    accuracy = correct/len(x_test)*100
+    return accuracy
+    
+
+
+def main(file, k, U):
+    df = pd.read_csv('Data4A1.tsv', sep='\t', header=0)
+    x = df.drop(['Sequence.id'], axis=1)
+    y = df['Class']
+    x_train, x_test, _, _ = train_test_split(x, y, test_size=int(U)/len(df), shuffle=True)
+    
+    accuracy = train(x_train, x_test, int(k))
+    print(f'{k}\t{accuracy}%')
+    
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        main('Data4A1.tsv', 5, 50)
+
+    # generate data for bonus
+    if BONUS == True:
+        df = pd.read_csv('Data4A1.tsv', sep='\t', header=0)
+        x = df.drop(['Sequence.id'], axis=1)
+        y = df['Class']
+        
+        results = dict()
+        for k in range(1, 16):
+            avg = 0
+            for i in range(0, 10): # 10 random shuffles for better avgs
+                x_train, x_test, _, _ = train_test_split(x,y,
+                                                         test_size=50/len(df),
+                                                         shuffle=True)
+                avg += train(x_train, x_test, k)
+            
+            print(f'{k}\t{avg/10}%')
+            results[k] = avg/10 
